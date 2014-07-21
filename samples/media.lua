f47c796c92588ef54790a4db3e169607abdb0df8
Steve Kemp <steve@steve.org.uk>
2014-07-21 12:53:55 +0100

    Correctly handle Xine.
     Xine will change its application/title depending on the media
    being played.

--- samples/media.lua
+++ samples/media.lua
@@ -1,10 +1,21 @@
 --
--- Media-players will default to "always on top"
+-- Media-players should default to being "always on top".
+--
+-- Here we cope with mplayer, totem, and xine.
 --
 --
 
-if ( ( window_application() == "xine" ) or
+
+--
+-- Xine will update its title/application/class to include the
+-- name of the media that is being played.
+--
+-- To cope with that we have to take the substring for comparison
+-- purposes.
+--
+if ( ( string.sub(window_application(), 0, 5) == "xine:" ) or
      ( window_application() == "MPlayer" ) or
      ( window_application() == "Totem Movie Player" ) ) then
    above()
 end
+
