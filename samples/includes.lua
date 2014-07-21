51bae0fa2ec4f089991a3590571e2d3f76737e67
Steve Kemp <steve@steve.org.uk>
2014-07-21 21:54:35 +0100

    Add an example.

---
+++ samples/includes.lua
@@ -0,0 +1,35 @@
+--
+-- This sample shows how you can include other files.
+--
+
+--
+-- The standard way of including a file, from Lua, will work
+-- just fine:
+--
+--    dofile( "/some/path/foo.lua" )
+--
+
+
+--
+-- However the more common approach is to read all files from
+-- a particular directory, such as ~/.kpie.d/
+--
+-- The following code does that.
+
+
+--
+-- Read the directory entries beneath ~/.kpie.d/
+--
+dir = os.getenv( "HOME" ) .. "/.kpie.d/"
+ents = readdir( dir )
+
+--
+-- If any of the files matches the pattern "*.lua" then load them.
+--
+for k,v in ipairs( ents ) do
+   if ( string.match( v, "\.lua$" ) ) then
+      if ( DEBUG ) then print( "Loading file: " .. dir .. v ) end
+      dofile( dir .. v )
+   end
+end
+
