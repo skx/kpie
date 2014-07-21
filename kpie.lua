19b5011492d9f53797d7ff6f4e206bc9beaff20c
Steve Kemp <steve@steve.org.uk>
2014-07-21 11:18:53 +0100

    Document string:lower and string:match

--- kpie.lua
+++ kpie.lua
@@ -12,7 +12,7 @@ end
 --
 -- If Xine is launched it should be "always on top"
 --
-if ( window_class() == "xine" ) then
+if ( window_application() == "xine" ) then
    above()
 end
 
@@ -24,3 +24,19 @@ if ( window_title() == "xlogo" ) then
    pin()
 end
 
+
+--
+-- This configuration file is Lua, so we can look for substrings too:
+--
+if ( string.find( window_title(), "emacs@" ) ) then
+   print( "Emacs is running and we like it!" )
+   print( "Window Title: " .. window_title() )
+end
+
+
+--
+-- As a final tip if you wish to change a string to lower-case you
+-- can use the following:
+--
+-- print ( window_title():lower() )
+--
