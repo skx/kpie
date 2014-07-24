fee304861afeda6a883c3fb82eb5acb8726e88c8
Steve Kemp <steve@steve.org.uk>
2014-07-24 17:18:15 +0100

    Added sample for showing how to center a window.
      This is just a demo, but will center all GNOME Terminal
     windows - perhaps not waht you want if you have multiple
     ones open, as they'll be on top of each other!

---
+++ samples/center.lua
@@ -0,0 +1,41 @@
+--
+-- Shows how to center a window.
+--
+-- Usage:
+--
+--    kpie [--single] ./center.lua
+--
+
+
+
+--
+-- This function centers a window
+--
+function center()
+
+   -- get the size of the window
+   width,height = size()
+
+   -- get the size of the screen
+   sx = screen_width()
+   sy = screen_height()
+
+   newx = (sx/2) - (width/2)
+   newy = (sy/2) - (height/2)
+
+   xy(newx,newy)
+
+   if ( DEBUG ) then
+      print( "Moving to " .. newx .. "," .. newy )
+   end
+end
+
+
+--
+--  We only process "Terminal" windows.
+--
+if ( ( window_title() == "Terminal" ) and
+     ( window_class() == "Terminal" ) and
+     ( window_application() == "Terminal" ) ) then
+   center()
+end
